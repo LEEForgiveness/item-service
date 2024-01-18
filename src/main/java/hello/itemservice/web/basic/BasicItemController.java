38e5e9ddf0b191e7908ba1f0e5a96899a1646a25
grang <leesln1@naver.com>
2024-01-18 19:22:57 +0900

    01.18

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -83,13 +84,33 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item, Model model){
         //@ModelAttribute자체도 생략 가능
         //@ModelAttribute("item")을 생략하면 Item클래스 이름을 소문자 item으로 인식함
         itemRepository.save(item);
 
         return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(Item item, Model model){
+        //@ModelAttribute자체도 생략 가능
+        //@ModelAttribute("item")을 생략하면 Item클래스 이름을 소문자 item으로 인식함
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, Model model, RedirectAttributes redirectAttributes){
+        //@ModelAttribute자체도 생략 가능
+        //@ModelAttribute("item")을 생략하면 Item클래스 이름을 소문자 item으로 인식함
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
